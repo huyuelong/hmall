eb7b94cfae6c9fc14306cadb6e7624e092024368
huyuelong <949910047@qq.com>
2025-04-24 00:27:45 +0800

    feat(cart-service): 用 Feign 客户端替换手动负载均衡逻辑
    - 在 CartApplication 中添加 @EnableFeignClients 注解- 创建 ItemClient 接口，使用 Feign 客户端调用 item-service
    - 在 CartServiceImpl 中注入 ItemClient，替换原有的手动负载均衡逻辑
    - 更新 pom.xml，添加 spring-cloud-starter-openfeign 和 spring-cloud-starter-loadbalancer 依赖

--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.client.ItemClient;
 import com.hmall.common.exception.BizIllegalException;
 import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
@@ -44,9 +45,11 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-    private final RestTemplate restTemplate;
+//    private final RestTemplate restTemplate;
+//
+//    private final DiscoveryClient discoveryClient;
 
-    private final DiscoveryClient discoveryClient;
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -93,7 +96,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
-        // 2.1根据服务名称获取服务的实例列表
+/*        // 2.1根据服务名称获取服务的实例列表
         List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
         if(CollUtil.isEmpty(instances)) {
             return;
@@ -101,7 +104,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         // 2.2手写负载均衡，从服务列表中挑选一个实例
         ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
 
-        // 2.1，利用RestTemplate发起http请求，得到http的响应
+        // 2.3，利用RestTemplate发起http请求，得到http的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
                 instance.getUri() + "/items?ids={ids}",
                 HttpMethod.GET,
@@ -115,7 +118,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
             // 查询失败，直接结束
             return;
         }
-        List<ItemDTO> items = response.getBody();
+        List<ItemDTO> items = response.getBody();*/
+
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
 
         if (CollUtils.isEmpty(items)) {
             return;
