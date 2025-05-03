36a7c962bf5bda02b1f917378de33009d68b84c4
huyuelong <949910047@qq.com>
2025-05-03 21:44:22 +0800

    feat(auth): 实现基于 JWT 的认证机制
    - 新增 AuthGlobalFilter 进行全局认证拦截- 添加 JwtProperties 配置类和 JwtTool 工具类- 在 CartController 中集成用户信息
    - 更新 Maven 依赖，添加 Spring Cloud Alibaba Nacos Config 和 Spring Cloud Starter Bootstrap

--- hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
+++ hm-api/src/main/java/com/hmall/api/config/DefaultFeignConfig.java
@@ -1,11 +1,26 @@
 package com.hmall.api.config;
 
+import com.hmall.common.utils.UserContext;
 import feign.Logger;
+import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 
 public class DefaultFeignConfig {
     @Bean
     public Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor userInfoInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(feign.RequestTemplate requestTemplate) {
+                Long userId = UserContext.getUser();
+                if (userId != null) {
+                    requestTemplate.header("user-info", userId.toString());
+                }
+            }
+        };
     }
 }
